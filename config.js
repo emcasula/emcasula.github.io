3556a4287191cc2847630aa5b3ee7c2a96f034ae
emcasula <dottoressaemanuelacasula@gmail.com>
2021-01-02 23:29:41 +0100

    fix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Dott.ssa Emanuela Casula',
   subHeading: 'Biologa Nutrizionista  ',
   // social
