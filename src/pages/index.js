f2239dd5ac8076b4e16e004cc47dba8e653fac69
emcasula <dottoressaemanuelacasula@gmail.com>
2021-01-02 23:41:09 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -156,8 +156,8 @@ const IndexPage = () => {
         </div>
 
         <div class="inner">
-          <form method="post" action="https://formsubmit.co/dottoressaemanuelacasula@gmail.com">
-          <input type="hidden" name="_next" value="/ThankYou"></input>
+          <form method="post" action="https://formsubmit.co/8a190bb170b130de0b8d01c967646eca">
+            <input type="hidden" name="_next" value="/ThankYou"></input>
             <div className="row gtr-uniform">
               <div className="col-6 col-12-xsmall">
                 <input type="text" name="name" id="name" defaultValue="" placeholder="Nome" />
@@ -169,7 +169,7 @@ const IndexPage = () => {
                 <textarea name="message" id="message" placeholder="Scrivi il tuo messaggio" rows="6"></textarea>
               </div>
               <div className="col-12-small">
-                <input type="checkbox" id="gdpr-consent" name="gdpr-consent" onClick={handleClick} checked={gdprChecked}/>
+                <input type="checkbox" id="gdpr-consent" name="gdpr-consent" onClick={handleClick} checked={gdprChecked} />
                 <label htmlFor="gdpr-consent">Dichiaro di aver letto e compreso l'informativa sulla privacy e acconsento al trattamento dei dati personali ai sensi dell'articolo 13 del Regolamento (UE) 2016/679.</label>
               </div>
               <div className="col-12">
@@ -177,7 +177,7 @@ const IndexPage = () => {
               </div>
               <div className="col-12">
                 <ul className="actions">
-                  <li><input type="submit" value="Invia Messaggio" className={`primary ${!gdprChecked ? "disabled": "enabled"}`}/></li>
+                  <li><input type="submit" value="Invia Messaggio" className={`primary ${!gdprChecked ? "disabled" : "enabled"}`} /></li>
                 </ul>
               </div>
             </div>
